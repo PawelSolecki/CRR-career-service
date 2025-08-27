f430f6d97e0655addf699f506003b306a765d41d
PawelS <58605803+PawelSolecki@users.noreply.github.com>
2025-08-27 14:14:12 +0200

    TheProtocolScrapper

---
+++ src/main/java/com/example/careerservice/scrapper/impl/TheProtocolScrapper.java
@@ -0,0 +1,61 @@
+package com.example.careerservice.scrapper.impl;
+
+import com.example.careerservice.scrapper.JobScrapper;
+import com.example.careerservice.scrapper.model.JobOffer;
+import com.example.careerservice.util.UrlValidator;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.List;
+
+public class TheProtocolScrapper implements JobScrapper {
+    @Override
+    public String getSourceName() {
+        return "theprotocol.it";
+    }
+
+    @Override
+    public JobOffer scrape(String url) {
+        try {
+            UrlValidator.validate(url, getSourceName());
+            Document doc = Jsoup.connect(url)
+                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.5993.90 Safari/537.36")
+                .referrer("https://www.google.com")
+                .timeout(10_000)
+                .get();
+            JobOffer jobOffer = parseDocument(doc);
+            jobOffer.setUrl(url);
+            return jobOffer;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private JobOffer parseDocument(Document doc) {
+        String company = doc.select("a[data-test=anchor-company-link]").first().ownText();
+        String title = doc.select("h1[data-test=text-offerTitle]").first().text();
+        String description = doc.select("div[data-test=PROJECT] div[data-test=text-sectionItem]").text();
+
+        List<String> technologies = doc.select(
+            "div[data-test=section-technologies] div[data-test=chip-technology]"
+        ).eachText();
+
+        List<String> requirements = doc.select(
+            "div[data-test=section-requirements] li"
+        ).eachText();
+
+        List<String> responsibilities = doc.select(
+            "div[data-test=section-responsibilities] li"
+        ).eachText();
+
+        return JobOffer.builder()
+            .company(company)
+            .title(title)
+            .description(description)
+            .technologies(technologies)
+            .requirements(requirements)
+            .responsibilities(responsibilities)
+            .build();
+    }
+}
---
+++ src/test/java/com/example/careerservice/scrapper/impl/TheProtocolScrapperTest.java
@@ -0,0 +1,50 @@
+package com.example.careerservice.scrapper.impl;
+
+import com.example.careerservice.scrapper.JobScrapper;
+import com.example.careerservice.scrapper.model.JobOffer;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TheProtocolScrapperTest {
+    private final JobScrapper scrapper = new TheProtocolScrapper();
+
+    @Test
+    void shouldReturnTheProtocolIt() {
+        assertEquals("theprotocol.it", scrapper.getSourceName());
+    }
+
+    @Nested
+    class ScrapeIntegrationTests {
+        private final static String OFFER = "https://theprotocol.it/szczegoly/praca/programista-java-warszawa,oferta,dbe30000-d4f2-5aeb-0e2e-08ddd4f20754?s=-4682230642&searchId=0dfd1446-6dd0-4866-88e3-4f1a1c59504e";
+
+        @Test
+        @Tag("integration")
+        void shouldReturnJobOffer() {
+            JobOffer offer = scrapper.scrape(OFFER);
+            assertNotNull(offer);
+            assertEquals(OFFER, offer.getUrl());
+            assertNotNull(offer.getTitle());
+            assertNotNull(offer.getCompany());
+            assertNotNull(offer.getDescription());
+            assertNotNull(offer.getTechnologies());
+            assertNotNull(offer.getRequirements());
+            assertNotNull(offer.getResponsibilities());
+        }
+
+        @Test
+        @Tag("integration")
+        void shouldThrowExceptionForInvalidUrl() {
+            assertThrows(IllegalArgumentException.class, () -> scrapper.scrape("https://www.noexample.com"));
+        }
+
+        @Test
+        @Tag("integration")
+        void shouldThrowRuntimeExceptionForNonexistentPage() {
+            assertThrows(RuntimeException.class, () -> scrapper.scrape("https://www.pracuj.pl/praca/nonexistent-page,oferta,0000000"));
+        }
+    }
+
+}
